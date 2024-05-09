79392075d95790be2fe459b9a9f9ff538b8fd0e2
moon_serface <simsim916@naver.com>
2024-05-09 18:15:01 +0900

    24-05-09

--- portfolio/src/page/career/Career_laon.jsx
+++ portfolio/src/page/career/Career_laon.jsx
@@ -12,8 +12,14 @@ const Career_laon = () => {
                 <i className={`fa-solid fa-caret-${open ? 'up' : 'down'}`}></i>
             </div>
             {open &&
-                <div>
-                    슈퍼바이저 - 직영, 가맹 매장 운영 & 관리
+                <div className='career_content'>
+                    <p>
+                        슈퍼바이저
+                    </p>
+                    <div className="career_detail">
+                        <p>삶에서 새로운 도전을 하고 싶어서 고향인 경기도 안양을 떠나서 강원도 원주에서 </p>
+                        <p>이후, <span>Photoshop 과 indesign 을 능숙하게 사용하여서 편집을 주로 담당</span>하게 되었고 직접 신문을 디자인하고, 인쇄소로 출력을 맡기는 등의 업무를 하였습니다.</p>
+                    </div>
                 </div>
             }
         </div>
