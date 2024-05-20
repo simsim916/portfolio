07fad3bdff537d000eeb0a6dd693a41974d93a50
Moon Desk <simsim916@naver.com>
2024-05-20 21:35:57 +0900

    240520

--- portfolio/src/page/nav/LeftBar.jsx
+++ portfolio/src/page/nav/LeftBar.jsx
@@ -14,7 +14,7 @@ const LeftBar = ({ navOpen, setNavOpen }) => {
     return (
         <div id='leftBar' className={navOpen ? null : 'close'}>
             <div id='update'>
-                last update 24.05.18
+                last update 24.05.181
             </div>
             <i onClick={() => setNavOpen(!navOpen)} className={`fa-solid fa-arrow-${navOpen ? 'left' : 'right'} minimize`}></i>
             {
