fc6dd9c4251cb7f9e75623d13822ba5b9e68f381
moon_serface <simsim916@naver.com>
2024-07-20 14:07:38 +0900

    24-07-20

--- portfolio/src/page/nav/LeftBar.jsx
+++ portfolio/src/page/nav/LeftBar.jsx
@@ -14,7 +14,7 @@ const LeftBar = ({ navOpen, setNavOpen }) => {
     return (
         <div id='leftBar' className={navOpen ? null : 'close'}>
             <div id='update'>
-                last update 24.05.30
+                last update 24.05.29
             </div>
             <i onClick={() => setNavOpen(!navOpen)} className={`fa-solid fa-arrow-${navOpen ? 'left' : 'right'} minimize`}></i>
             {
